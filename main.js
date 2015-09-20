62fc1fa424d7790a326225ca82743ed23c05fdfc
sytkm <kame.adventure@gmail.com>
2015-09-21 00:21:54 +0900

    スタミナ式修正、他bugfix
    index.html:
        -経験値・スタミナ・同僚の表示形式をtableに変更
        -jQueryをローカルファイルからCDNに変更
    main.js:
        -経験値の式を修正
        -スタミナ式をif条件分岐からfor文に変更
        -index.htmlをtableに変更したのに合わせappendからhtmlに変更

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 $(function(){
 	function CalcLevel(level){
-		var exp,stam,coll;
-		exp = level * 20 + 10;
-		stam = 40;
-		if(level < 20){
-			stam += parseInt(level / 2);
-		}else if(level < 50){
-			stam += parseInt((level - 20) / 3);
-		}else if(level < 90){
-			stam += parseInt((level - 50) / 4);
-		}else if(level < 130){
-			stam += parseInt((level - 90) / 5);
-		}else{
-			stam += parseInt((level - 130) / 6);
+		var exp,stam,coll,i,count;
+		exp = (level < 50)? (level * 20 + 10):((level-50) * 20 + 1500);
+		stam=40;
+		for(i=1,count=0;level-count>0;count=count+(i+1)*10,i++){
+			stam += ((level < (count + (i+1)*10))?parseInt((level-count)/(i+1)):10);
 		}
 		coll = ((level>5) ? (21 + parseInt(level / 10)) : 20);
 		return {"exp":exp,"stamina":stam,"colleague":coll};
@@ -21,8 +13,8 @@ $(function(){
 	$("div#calc").click(function(){
 		var level = parseInt($('#level').val());
 		var result = CalcLevel(level);
-		$('#resexp').append(result.exp);
-		$('#resstam').append(result.stamina);
-		$('#rescoll').append(result.colleague);
+		$('#resexp').html(result.exp);
+		$('#resstam').html(result.stamina);
+		$('#rescoll').html(result.colleague);
 	});
 });
