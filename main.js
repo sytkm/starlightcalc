fa4e6f567ae08105b1a87156feb5fe63bb1af9c0
sytkm <kame.adventure@gmail.com>
2015-09-22 00:08:01 +0900

    bootstrap他css、入力フォーム追加
    index.html:
        -bootstrapをlink
        -bootstrapでレスポンシブ化
        -入力フォーム(現在の経験値、現在のスタミナ)追加
        -タイトル追加
    main.js:
        -表示経験値を残り経験値に変更
        -levelに何も入力してない時に経験値だけ0になるバグを修正
    main.css:
        -テーブルにcssを使用
        -タイトル、tableをセンタリング

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(function(){
 	function CalcLevel(level){
 		var exp,stam,coll,i,count;
-		exp = (level < 50)? (level * 20 + 10):((level-50) * 20 + 1500);
-		stam=40;
-		for(i=1,count=0;level-count>0;count=count+(i+1)*10,i++){
-			stam += ((level < (count + (i+1)*10))?parseInt((level-count)/(i+1)):10);
+		exp = (level > 50) ? ((level-50) * 20 + 1500):(level * 20 + 10);
+		for(stam=40,i=1,count=0;level-count>0;count=count+(i+1)*10,i++){
+			stam += (level < (count + (i+1)*10))?parseInt((level - count)/(i+1)):10;
 		}
-		coll = ((level>5) ? (21 + parseInt(level / 10)) : 20);
+		coll = (level>5) ? (21 + parseInt(level / 10)) : 20;
 		return {"exp":exp,"stamina":stam,"colleague":coll};
 	}
 
-	$("div#calc").click(function(){
+	$("#calc").click(function(){
 		var level = parseInt($('#level').val());
-		var result = CalcLevel(level);
-		$('#resexp').html(result.exp);
+		var nowexp = parseInt($('#nowexp').val());
+		var result = CalcLevel(level?level:0);
+		$('#resexp').html(result.exp-(nowexp?nowexp:0));
 		$('#resstam').html(result.stamina);
 		$('#rescoll').html(result.colleague);
 	});
